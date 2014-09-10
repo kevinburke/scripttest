1135595d721212ecc551396b852dd1f100a42b9d
Kevin Burke <kev@inburke.com>
2014-09-10 10:08:54 -0700

    increase timeout

--- scripttest.py
+++ scripttest.py
@@ -254,7 +254,7 @@ class TestFileEnvironment(object):
                                     env=clean_environ(self.environ))
             import time
             import signal
-            time.sleep(10)
+            time.sleep(15)
             proc.send_signal(signal.SIGINT)
         else:
             proc = subprocess.Popen(all, stdin=subprocess.PIPE,
